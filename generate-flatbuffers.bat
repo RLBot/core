0a12e6551175e89ef7acd71a9a0029517cf81c3c
ddthj <theddthj@gmail.com>
2024-01-05 15:28:08 -0800

    Fixed generate-flatbuffers.bat
    Blame Virx for not using windows... or testing code

--- generate-flatbuffers.bat
+++ generate-flatbuffers.bat
@@ -1,7 +1,6 @@
 echo Generating flatbuffers header file...
 
-..\flatbuffers-schema\flatc.exe --csharp --gen-object-api --gen-onefile -o .\ ..\flatbuffers-schema\rlbot.fbs
+.\RLBotCS\flatbuffers-schema\flatc.exe --csharp --gen-object-api --gen-onefile -o .\RLBotCS\FlatBuffer .\RLBotCS\flatbuffers-schema\rlbot.fbs
 
 REM the file produced is called rlbot_generated.cs, rename it to rlbot.cs after removing the old one
-DEL ./RLBotCS/FlatBuffer/rlbot.cs
-ren ./RLBotCS/FlatBuffer/rlbot_generated.cs rlbot.cs
+ren .\RLBotCS\FlatBuffer\rlbot_generated.cs rlbot.cs
