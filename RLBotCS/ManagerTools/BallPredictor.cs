d5aacaaa8a056cf68b3a489d5ef8f5f23306d300
VirxEC <virx@virxcase.dev>
2024-07-05 23:18:19 -0400

    Much better heatseeker ball prediction

--- RLBotCS/ManagerTools/BallPredictor.cs
+++ RLBotCS/ManagerTools/BallPredictor.cs
@@ -47,11 +47,20 @@ public static partial class BallPredictor
     [LibraryImport("rl_ball_sym", EntryPoint = "load_standard_throwback")]
     private static partial void LoadStandardThrowback();
 
+    [LibraryImport("rl_ball_sym", EntryPoint = "get_heatseeker_target_y")]
+    private static partial float GetHeatseekerTargetY();
+
+    [LibraryImport("rl_ball_sym", EntryPoint = "reset_heatseeker_target")]
+    private static partial void ResetHeatseekerTarget();
+
     [LibraryImport("rl_ball_sym", EntryPoint = "set_heatseeker_target")]
     private static partial void SetHeatseekerTarget(byte blueGoal);
 
     [LibraryImport("rl_ball_sym", EntryPoint = "step")]
-    private static partial BallSlice Step(BallSlice ball);
+    private static unsafe partial BallSlice* Step(BallSlice ball, ushort ticks);
+
+    [LibraryImport("rl_ball_sym", EntryPoint = "free_ball_slices")]
+    private static unsafe partial void FreeBallSlices(BallSlice* slices, ushort ticks);
 
     private static Vec3 ToVec3(Bridge.Models.Phys.Vector3 vec) => new(vec.X, vec.Y, vec.Z);
 
@@ -99,43 +108,69 @@ public static partial class BallPredictor
         };
     }
 
-    public static BallPredictionT Generate(PredictionMode mode, float time, Ball currentBall)
+    public static BallPredictionT Generate(PredictionMode mode, float currentTime, Ball currentBall)
     {
         BallSlice ball =
             new()
             {
-                Time = time,
+                Time = currentTime,
                 Location = ToVec3(currentBall.Physics.Location),
                 LinearVelocity = ToVec3(currentBall.Physics.Velocity),
                 AngularVelocity = ToVec3(currentBall.Physics.AngularVelocity)
             };
 
-        const int numSeconds = 8;
-        const int numSlices = numSeconds * 120;
+        const ushort numSeconds = 8;
+        const ushort numSlices = numSeconds * 120;
 
         BallPredictionT ballPrediction = new() { Slices = new List<PredictionSliceT>(numSlices) };
 
         if (mode == PredictionMode.Heatseeker)
-            // Target goal is the opposite of the last touch
-            SetHeatseekerTarget(currentBall.LatestTouch.Team == 0 ? (byte)1 : (byte)0);
-
-        for (int i = 0; i < numSlices; i++)
         {
-            ball = Step(ball);
+            if (currentBall.LatestTouch.TimeSeconds < float.Epsilon)
+            {
+                // A goal happened, we're in kickoff
+                ResetHeatseekerTarget();
+            }
+            else if (currentTime - currentBall.LatestTouch.TimeSeconds < 0.1)
+            {
+                // Target goal is the opposite of the last touch
+                SetHeatseekerTarget(currentBall.LatestTouch.Team == 1 ? (byte)1 : (byte)0);
+            }
+            else if (GetHeatseekerTargetY() == 0 || MathF.Abs(ball.Location.Y) >= 4820)
+            {
+                // We're very likely to hit a wall that will redirect the ball towards the other goal
+                SetHeatseekerTarget(
+                    MathF.Abs(MathF.Sign(ball.LinearVelocity.Y) + 1) < float.Epsilon ? (byte)1 : (byte)0
+                );
+            }
+        }
 
-            PredictionSliceT slice =
-                new()
-                {
-                    GameSeconds = ball.Time,
-                    Physics = new PhysicsT
+        unsafe
+        {
+            var ballSlices = Step(ball, numSlices);
+            if (ballSlices == null)
+                return ballPrediction;
+
+            for (int i = 0; i < numSlices; i++)
+            {
+                BallSlice rawSlice = ballSlices[i];
+
+                PredictionSliceT slice =
+                    new()
                     {
-                        Location = ToVector3T(ball.Location),
-                        Velocity = ToVector3T(ball.LinearVelocity),
-                        AngularVelocity = ToVector3T(ball.AngularVelocity)
-                    }
-                };
+                        GameSeconds = rawSlice.Time,
+                        Physics = new PhysicsT
+                        {
+                            Location = ToVector3T(rawSlice.Location),
+                            Velocity = ToVector3T(rawSlice.LinearVelocity),
+                            AngularVelocity = ToVector3T(rawSlice.AngularVelocity)
+                        }
+                    };
 
-            ballPrediction.Slices.Add(slice);
+                ballPrediction.Slices.Add(slice);
+            }
+
+            FreeBallSlices(ballSlices, numSlices);
         }
 
         return ballPrediction;
--- RLBotCSTests/BallPrediction.cs
+++ RLBotCSTests/BallPrediction.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Bridge.Models.Phys;
 using Bridge.State;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,26 +9,67 @@ namespace RLBotCSTests
     [TestClass]
     public class BallPrediction
     {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
         [TestMethod]
         public void TestBallPred()
         {
             BallPredictor.SetMode(PredictionMode.Standard);
 
             var packet = new GameState();
-            BallPredictor.Generate(PredictionMode.Standard, 1, packet.Ball);
+            packet.Balls[12345] = new();
 
-            packet.Ball.Physics = new Physics(
-                new Vector3(0, 0, 100),
-                new Vector3(600, 600, 100),
-                new Vector3(1, 2, 0.5F),
+            BallPredictor.Generate(PredictionMode.Standard, 1, packet.Balls[12345]);
+
+            packet.Balls[12345].Physics = new Physics(
+                new Vector3(0, 0, 1.1f * 91.25f),
+                new Vector3(600, 1550, 0),
+                new Vector3(0, 0, 0),
                 new Rotator(0, 0, 0)
             );
 
-            var ballPred = BallPredictor.Generate(PredictionMode.Standard, 1, packet.Ball);
+            var ballPred = BallPredictor.Generate(PredictionMode.Standard, 1, packet.Balls[12345]);
 
             int numSlices = 8 * 120;
             Assert.AreEqual(numSlices, ballPred.Slices.Count);
             Assert.IsTrue(ballPred.Slices[numSlices - 1].GameSeconds > 8.9999);
+
+            // comment out to see results of the below test
+            // dotnet test -c "Release" for best results
+            return;
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            int numIterations = 20_000;
+            for (int i = 0; i < numIterations; i++)
+            {
+                packet.Balls[12345].Physics = new Physics(
+                    new Vector3(0, 0, 1.1f * 91.25f),
+                    new Vector3(600, 1550, 0),
+                    new Vector3(0, 0, 0),
+                    new Rotator(0, 0, 0)
+                );
+
+                stopWatch.Start();
+                BallPredictor.Generate(PredictionMode.Standard, 1, packet.Balls[12345]);
+                stopWatch.Stop();
+            }
+
+            float averageTime = (float)stopWatch.ElapsedMilliseconds / numIterations;
+            TestContext.WriteLine("Average time to generate ball prediction: " + averageTime + "ms");
+
+            // makes the above result print out
+            Assert.IsTrue(false);
         }
     }
 }
