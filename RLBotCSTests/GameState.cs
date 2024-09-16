6e2420a603b518ba2af95fc41417503941527a33
VirxEC <virx@virxcase.dev>
2024-09-16 13:49:09 -0400

    New test

--- RLBotCSTests/GameState.cs
+++ RLBotCSTests/GameState.cs
@@ -1,3 +1,4 @@
+using Bridge.Packet;
 using Bridge.State;
 using Google.FlatBuffers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -17,5 +18,23 @@ public class TestGameState
         FlatBufferBuilder builder = new(1024);
         var offset = GameTickPacket.Pack(builder, packet.ToFlatBuffers());
         builder.Finish(offset.Value);
+
+        packet.BoostPads.Add(0, new BoostPadInfo {
+            SpawnPosition = new Bridge.Models.Phys.Vector3(1, 1, 0),
+            IsActive = true
+        });
+
+        packet.BoostPads.Add(1, new BoostPadInfo {
+            SpawnPosition = new Bridge.Models.Phys.Vector3(0, 0, 0),
+            IsActive = true
+        });
+
+        packet.BoostPads.Add(2, new BoostPadInfo {
+            SpawnPosition = new Bridge.Models.Phys.Vector3(0, 1, 0),
+            IsActive = true
+        });
+
+        var flatPacket = packet.ToFlatBuffers();
+        Assert.AreEqual(3, flatPacket.BoostPadStates.Count);
     }
 }
