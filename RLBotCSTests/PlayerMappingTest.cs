1a0bb93144b67c9076dc816afb4e9717d9a6fb8b
Virx <virx@virxcase.dev>
2024-12-14 19:28:53 -0500

    Update `Bridge.dll`
    Comments for `PlayerMappingTest`

--- RLBotCSTests/PlayerMappingTest.cs
+++ RLBotCSTests/PlayerMappingTest.cs
@@ -32,8 +32,10 @@ public class PlayerMappingTest
             IsBot = true
         };
 
+        // add pending spawn
         _playerMapping.AddPendingSpawn(spawnTracker);
 
+        // apply car spawn from known player
         var metadata = _playerMapping.ApplyCarSpawn(actorId, commandId);
 
         Assert.AreEqual(desiredIndex, _playerMapping.PlayerIndexFromActorId(actorId));
@@ -42,12 +44,14 @@ public class PlayerMappingTest
         Assert.IsTrue(metadata.IsBot);
         Assert.IsTrue(!metadata.IsCustomBot);
 
+        // apply car spawn from unknown player
         var metadata2 = _playerMapping.ApplyCarSpawn(111, 222);
         uint? index = _playerMapping.PlayerIndexFromActorId(111);
 
         Assert.AreEqual(0u, _playerMapping.PlayerIndexFromActorId(111));
         Assert.IsNotNull(index);
-        Assert.AreEqual(index, 0u);
+        Assert.AreEqual(0u, index);
+        Assert.IsNull(metadata2.SpawnId);
         Assert.AreEqual(desiredIndex, _playerMapping.PlayerIndexFromActorId(actorId));
         Assert.IsTrue(!metadata2.IsBot);
         Assert.IsTrue(!metadata2.IsCustomBot);
@@ -55,6 +59,5 @@ public class PlayerMappingTest
         uint? index2 = _playerMapping.PlayerIndexFromActorId(456);
 
         Assert.IsNull(index2);
-        Assert.AreNotEqual(index2, 0u);
     }
 }
