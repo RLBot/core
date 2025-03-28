0e0078d7c7d817d86efc610ba30758f790e4b02c
Virx <virx@virxcase.dev>
2025-03-28 11:55:58 -0400

    Bump version

--- RLBotCS/Main.cs
+++ RLBotCS/Main.cs
@@ -8,7 +8,7 @@ using RLBotCS.Server.ServerMessage;
 
 if (args.Length > 0 && args[0] == "--version")
 {
-    Console.WriteLine("RLBotServer v5.beta.4.7");
+    Console.WriteLine("RLBotServer v5.beta.4.9");
     Environment.Exit(0);
 }
 
