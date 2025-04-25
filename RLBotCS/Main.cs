c2be2b3335c100d251d74a04812e5cd571679fc7
Virx <virx@virxcase.dev>
2025-04-25 16:04:15 -0400

    Set gravity for ball prediction

--- RLBotCS/Main.cs
+++ RLBotCS/Main.cs
@@ -8,7 +8,7 @@ using RLBotCS.Server.ServerMessage;
 
 if (args.Length > 0 && args[0] == "--version")
 {
-    Console.WriteLine("RLBotServer v5.beta.6.3");
+    Console.WriteLine("RLBotServer v5.beta.6.4");
     Environment.Exit(0);
 }
 
