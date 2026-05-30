b0e284d6bbccaf9bffe6345d5bc18d5937187f42
Veeno <veeno00@gmail.com>
2026-05-31 01:37:32 +0200

    Optimize RectUtil.LessThanGeoMean (m.b is always greater than m.a).

--- RLBotCS/ManagerTools/RectUtil.cs
+++ RLBotCS/ManagerTools/RectUtil.cs
@@ -71,7 +71,7 @@ public static class RectUtil
 
     private static bool LessThanGeoMean(float x, (float a, float b) m)
     {
-        if (m.a >= HalfPrecisionRangeHigh || m.b >= HalfPrecisionRangeHigh)
+        if (m.b >= HalfPrecisionRangeHigh)
             return x < MathF.Sqrt(m.a) * MathF.Sqrt(m.b);
         return x * x < m.a * m.b;
     }
