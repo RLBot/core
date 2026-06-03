f192701c575e7580bad0b711eab4b58d25de4322
Veeno <veeno00@gmail.com>
2026-06-03 13:57:49 +0200

    Implement a proper practical unit test for RectUtil and make it actually work right.

--- RLBotCSTests/ManagerTools/RectUtilTest.cs
+++ RLBotCSTests/ManagerTools/RectUtilTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RLBotCS.ManagerTools;
 
 namespace RLBotCSTests.ManagerTools;
@@ -6,16 +7,21 @@ namespace RLBotCSTests.ManagerTools;
 [TestClass]
 public class RectUtilTest
 {
-    const uint TestUpTo = 64;
+    private static (float, float) RoundingBounds(uint n)
+    {
+        return (MathF.BitIncrement(n - 0.5f), MathF.BitDecrement(n + 0.5f));
+    }
 
     [TestMethod]
-    public void ApproximateRectTest()
+    public void ApproximateRectTest_Generic()
     {
+        const uint TestUpTo = 64;
+
         for (uint i = 1; i <= TestUpTo; ++i)
         {
             for (uint j = 1; j <= TestUpTo; ++j)
             {
-                (ushort cols, ushort rows, float scale) = RectUtil.ApproximateRect(i, i, j, j);
+                (ushort cols, ushort rows, float scale) = RectUtil.RectSolve(i, i, j, j);
                 Assert.AreEqual(1, cols);
                 Assert.AreEqual(1, rows);
                 // Slightly iffy, but it passes.
@@ -25,26 +31,49 @@ public class RectUtilTest
 
         for (uint i = 1; i <= TestUpTo; ++i)
         {
-            float iMin = i * 0.96f;
-            float iMax = i * 1.04f;
+            (float iMin, float iMax) = RoundingBounds(i);
             for (uint j = 1; j <= TestUpTo; ++j)
             {
-                float jMin = j * 0.96f;
-                float jMax = j * 1.04f;
+                (float jMin, float jMax) = RoundingBounds(j);
                 for (uint k = 1; k <= TestUpTo; ++k)
                 {
                     for (uint l = 1; l <= TestUpTo; ++l)
                     {
-                        (ushort cols, ushort rows, float scale) = RectUtil.ApproximateRect(
+                        (ushort cols, ushort rows, float scale) = RectUtil.RectSolve(
                             i,
                             j,
                             k,
                             l
                         );
+                        Assert.IsLessThan(Rendering.RectangleStringMaxLength + 1, cols + rows);
                         Assert.IsInRange(iMin, iMax, k * scale * cols);
                         Assert.IsInRange(jMin, jMax, l * scale * rows);
                     }
                 }
+            }
+        }
+    }
+
+    [TestMethod]
+    public void ApproximateRectTest_Practical()
+    {
+        const uint TestUpTo = 7680;
+
+        for (uint i = 1; i <= TestUpTo; ++i)
+        {
+            (float iMin, float iMax) = RoundingBounds(i);
+            for (uint j = 1; j <= TestUpTo; ++j)
+            {
+                (float jMin, float jMax) = RoundingBounds(j);
+                (ushort cols, ushort rows, float scale) = RectUtil.RectSolve(
+                    i,
+                    j,
+                    Rendering.FontWidthPixels,
+                    Rendering.FontHeightPixels
+                );
+                Assert.IsLessThan(Rendering.RectangleStringMaxLength + 1, cols + rows);
+                Assert.IsInRange(iMin, iMax, Rendering.FontWidthPixels * scale * cols);
+                Assert.IsInRange(jMin, jMax, Rendering.FontHeightPixels * scale * rows);
             }
         }
     }
