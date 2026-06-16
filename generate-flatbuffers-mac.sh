e993d58d3fd48c79dfa77209938d02604bfd19d5
VirxEC <virx@virxcase.dev>
2026-06-16 11:33:33 -0500

    Add FlatBuffers generation for macOS

---
+++ generate-flatbuffers-mac.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+echo Generating flatbuffers header file...
+
+./flatbuffers-schema/binaries/flatc_mac --gen-all --csharp --gen-object-api --gen-onefile -o ./FlatBuffer ./flatbuffers-schema/schema/rlbot.fbs
+
+# the file produced is called rlbot_generated.cs, rename it to RLBot.cs after removing the old one
+rm -f ./FlatBuffer/RLBot.cs
+mv ./FlatBuffer/rlbot_generated.cs ./FlatBuffer/RLBot.cs
+sed -i '' 's/rlbot\.flat/RLBot.Flat/g' ./FlatBuffer/RLBot.cs
+
+echo Done.
