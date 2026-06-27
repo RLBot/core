256d31c76e257515216daed796e3d87cc09cd064
NicEastvillage <nicoesterby@gmail.com>
2026-06-27 14:09:25 +0200

    Formatting

--- RLBotCS/ManagerTools/LaunchManager/Epic.Windows.cs
+++ RLBotCS/ManagerTools/LaunchManager/Epic.Windows.cs
@@ -60,7 +60,9 @@ public static partial class LaunchManager
         epicRocketLeague.StartInfo.RedirectStandardOutput = true;
         epicRocketLeague.StartInfo.RedirectStandardError = true;
 
-        Logger.LogInformation($"Launching Rocket League via Epic: \"{directGamePath} {rlbotArgs}\"...");
+        Logger.LogInformation(
+            $"Launching Rocket League via Epic: \"{directGamePath} {rlbotArgs}\"..."
+        );
         Logger.LogDebug(
             $"Full command: {epicRocketLeague.StartInfo.FileName} {epicRocketLeague.StartInfo.Arguments}"
         );
